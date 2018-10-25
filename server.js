b47375858350de8efb2b0c5400fb0b36d9eeeb13
Fdebijl <floris.debijl@live.nl>
2018-10-25 18:29:08 +0200

    Check for title changes only within same categories
    This prevents the double posting that's been happening

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const PURGE_TRESHOLD = 48; //hours
 const ROOT = "http://feeds.nos.nl/";
 const FEEDS = [
   "nosnieuwsbinnenland"
+  ,"nosnieuwsalgemeen"
   ,"nosnieuwsbuitenland"
   ,"nosnieuwspolitiek"
   ,"nosnieuwseconomie"
@@ -43,9 +44,9 @@ let articles = [];
 
 
 // Run the article scraper every SCRAPER_INTERVAL seconds
-retrieveAndCheckArticles();
+retrieveArticles();
 setInterval(() => {
-  retrieveAndCheckArticles();
+  retrieveArticles();
 }, SCRAPER_INTERVAL * 1000)
 
 // Run The Purge (2013) every PURGE_INTERVAL hours
@@ -61,54 +62,51 @@ setInterval(() => {
 
 
 // Main loop - check for new articles in all feeds
-async function retrieveAndCheckArticles() {
-  console.log("======================================================================")
-  let items = [];
-
-  // Aggregate all NOS feeds into one array of items
+async function retrieveArticles() {
   for (FEEDINDEX in FEEDS) {
-    console.log(`Gathering RSS feed ${FEEDS[FEEDINDEX]}`);
     let feed = await parser.parseURL(ROOT + FEEDS[FEEDINDEX]);
-    telemetry.histogram(FEEDS[FEEDINDEX], feed.items.length);
-    items.push(...feed.items);
+    let category = feed.title.replace(/(NOS(\.nl)?)|(nieuws)/gi, '').capitalize();
+    telemetry.histogram(category, feed.items.length);
+    feed.items.forEach(item => {
+      checkArticleForNewTitle(item, category);
+    });
   }
 
-  // Remove duplicates and ping telemetry with the amount of articles we encountered
-  items = removeDuplicatesFrom(items);
-  telemetry.histogram('feeditem_count', items.length);
+  doAfterCheck();
+}
 
-  items.forEach(item => {
-    // Article exists, check if title has changed since the last time we saw it
-    let existingArticle = articles.find((article, index, array) => {
-      return article.guid == item.guid;
-    });
+function checkArticleForNewTitle(newArticle, injectCategory) {
+  // Inject category so we can display it later
+  newArticle.category = injectCategory
 
-    if (!!existingArticle) {
-      if (existingArticle.title != item.title) {
-        // Title has changed, send out a Tweet
-        notifyTitleChanged(item, existingArticle);
-
-        // Set old article to new info, otherwise the article would trigger notifyTitleChanged again in the next cycle
-        console.log(`Replacing article at index ${articles.indexOf(existingArticle)} in collection.`)
-        articles[articles.indexOf(existingArticle)] = item;
-      }
-
-      // Title hasn't changed and we already have the article in our collection
-      telemetry.increment('article_stale');
-    } else {
-      // New item, add it to our collection
-      articles.push(item);
-      telemetry.increment('article_new');
-    }
+  // Article exists, check if title has changed since the last time we saw it
+  let existingArticle = articles.find((article, index, array) => {
+    return (article.guid == newArticle.guid && article.category == newArticle.guid);
   });
 
-  afterCheck();
+  if (!!existingArticle) {
+    if ((existingArticle.title != newArticle.title) && (existingArticle.category == newArticle.category)) {
+      // Title has changed, send out a Tweet
+      notifyTitleChanged(newArticle, existingArticle);
+
+      // Set old article to new info, otherwise the article would trigger notifyTitleChanged again in the next cycle
+      console.log(`Replacing article at index ${articles.indexOf(existingArticle)} in collection.`)
+      articles[articles.indexOf(existingArticle)] = newArticle;
+    }
+
+    // Title hasn't changed and we already have the article in our collection
+    telemetry.increment('article_stale');
+  } else {
+    // New (unseen) article, add it to our collection
+    articles.push(newArticle);
+    telemetry.increment('article_new');
+  }
 }
 
 // Send tweet that the title of an article has changed
 function notifyTitleChanged(newArticle, existingArticle) {
   telemetry.increment('article_changed');
-  let statusText = `De kop "${existingArticle.title.trim()}" is zojuist gewijzigd naar "${newArticle.title.trim()}" ${newArticle.guid}`;
+  let statusText = `[${newArticle.category}] De kop "${existingArticle.title.trim()}" is zojuist gewijzigd naar "${newArticle.title.trim()}" ${newArticle.guid}`;
   let params = { 
     status: statusText
   }
@@ -122,7 +120,7 @@ function notifyTitleChanged(newArticle, existingArticle) {
       telemetry.increment('error_rate');
     } else {
       telemetry.check("tweet_result", telemetry.CHECKS.OK);
-      console.log(`Sent out a tweet: ${statusText}`);
+      console.log(`Sent out a tweet: ${statusText} -- Oldcat: ${existingArticle.source}, newcat: ${newArticle.source}`);
     }
   })
 }
@@ -145,15 +143,11 @@ function purgeArticles() {
   console.log(`Purged ${countPurged} articles (${Math.floor(countPurged / total * 100)}%)`)
 }
 
-function afterCheck() {
+function doAfterCheck() {
   // Update article count
   telemetry.histogram('article_count', articles.length);
-  articles = removeDuplicatesFrom(articles);
 }
 
-function removeDuplicatesFrom(array) {
-  let seen = {};
-  return array.filter(function(item) {
-      return array.hasOwnProperty(item.guid) ? false : (seen[item.guid] = true);
-  });
-}
+String.prototype.capitalize = function() {
+  return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
