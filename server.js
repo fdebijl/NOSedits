efa28f4746611bf84f9c011dd596fcb4c5532194
Fdebijl <floris.debijl@live.nl>
2018-10-28 11:41:36 +0100

    Improved readability, offload config to json and reply-to
    Additional changes to the same title will now be replied to the previous change

--- server.js
+++ server.js
@@ -1,91 +1,71 @@
 require('dotenv').config();
 
 const Parser = require('rss-parser'),
-      parser = new Parser(),
       moment = require('moment'),
+      fs = require('fs'),
       Twit = require('twit'),
-      config = {
-        twitter: {
-          consumer_key: process.env.CONSUMER_KEY,
-          consumer_secret: process.env.CONSUMER_SECRET,
-          access_token: process.env.ACCESS_TOKEN,
-          access_token_secret: process.env.ACCESS_TOKEN_SECRET
-        }
-      };
+      StatsD = require('hot-shots');
 
-const T = new Twit(config.twitter);
-const StatsD = require('hot-shots');
-let dd_options = {
+const parser = new Parser();
+const T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+const telemetry = new StatsD({
   prefix: "nosedits-"
-};
-const telemetry = new StatsD(dd_options);
+});
 
-const SCRAPER_INTERVAL = 30; //seconds
-const TELEMETRY_INTERVAL = 60; //seconds
-const PURGE_INTERVAL = 24; //hours
-const PURGE_TRESHOLD = 48; //hours
+// Load config
+let CONFIG;
+CONFIG = JSON.parse(fs.readFileSync('config.json'));
+let NO_TELEMETRY = CONFIG.NO_TELEMETRY;
 
-// All NOS feeds to cycle through
-const ROOT = "http://feeds.nos.nl/";
-const FEEDS = [
-  "nosnieuwsbinnenland"
-  ,"nosnieuwsalgemeen"
-  ,"nosnieuwsbuitenland"
-  ,"nosnieuwspolitiek"
-  ,"nosnieuwseconomie"
-  ,"nosnieuwscultuurenmedia"
-  ,"nosnieuwstech"
-  ,"nosnieuwskoningshuis"
-];
-
-// Maintain a collection of all articles
+// The collection of articles
 let articles = [];
 
-
-
-// Run the article scraper every SCRAPER_INTERVAL seconds
-retrieveArticles();
 setInterval(() => {
   retrieveArticles();
-}, SCRAPER_INTERVAL * 1000)
+}, CONFIG.SCRAPER_INTERVAL * 1000)
 
-// Run The Purge (2013) every PURGE_INTERVAL hours
 setInterval(() => {
   purgeArticles();
-}, PURGE_INTERVAL * 60 * 60 * 1000);
+}, CONFIG.PURGE_INTERVAL * 60 * 60 * 1000);
 
-// Ping the telemetry server every TELEMETRY_INTERVAL seconds so it knows we're still alive
 setInterval(() => {
+  // Ping the telemetry server every TELEMETRY_INTERVAL seconds so it knows we're still alive
   telemetry.check('service.up', telemetry.CHECKS.OK);
   
-  for (FEEDINDEX in FEEDS) {
-    let category = FEEDS[FEEDINDEX].replace(/(NOS(\.nl)?)|(nieuws)/gi, '').capitalize();
+  // Count the number of articles from each category
+  for (FEEDINDEX in CONFIG.FEEDS) {
+    let category = CONFIG.FEEDS[FEEDINDEX].catStrip();
     let inCategoryCount = articles.filter(article => {
       return article.category == category;
     });
     
     telemetry.histogram(category, inCategoryCount.length);
   }
-}, TELEMETRY_INTERVAL * 1000);
-
-
+}, CONFIG.TELEMETRY_INTERVAL * 1000);
 
 // Main loop - check for new articles in all feeds
 async function retrieveArticles() {
   let totalLength = 0;
 
-  for (FEEDINDEX in FEEDS) {
-    let feed = await parser.parseURL(ROOT + FEEDS[FEEDINDEX]);
-    let category = feed.title.replace(/(NOS(\.nl)?)|(nieuws)/gi, '').capitalize().trim();
+  for (FEEDINDEX in CONFIG.FEEDS) {
+    let feed = await parser.parseURL(CONFIG.ROOT + CONFIG.FEEDS[FEEDINDEX]);
+    let category = feed.title.catStrip();
     totalLength = totalLength + feed.items.length;
     feed.items.forEach(item => {
+      // Only proceed if category is present
       if (category) {
         checkArticleForNewTitle(item, category);
       }
     });
   }
   
-  doAfterCheck(totalLength);
+  telemetry.histogram('article_count', articles.length);
+  telemetry.histogram('feeditems_count', totalLength);
 }
 
 function checkArticleForNewTitle(newArticle, injectCategory) {
@@ -93,18 +73,14 @@ function checkArticleForNewTitle(newArticle, injectCategory) {
   newArticle.category = injectCategory
 
   // Article exists, check if title has changed since the last time we saw it
-  let existingArticle = articles.find((article, index, array) => {
+  let existingArticle = articles.find(article => {
     return (article.guid == newArticle.guid && article.category == newArticle.category);
   });
 
   if (!!existingArticle) {
     if ((existingArticle.title != newArticle.title) && (existingArticle.category == newArticle.category)) {
       // Title has changed, send out a Tweet
-      notifyTitleChanged(newArticle, existingArticle);
-
-      // Set old article to new info, otherwise the article would trigger notifyTitleChanged again in the next cycle
-      console.log(`Replacing article at index ${articles.indexOf(existingArticle)} in collection.`)
-      articles[articles.indexOf(existingArticle)] = newArticle;
+      notifyTitleChanged(newArticle, existingArticle); 
     }
 
     // Title hasn't changed and we already have the article in our collection
@@ -119,10 +95,20 @@ function checkArticleForNewTitle(newArticle, injectCategory) {
 // Send tweet that the title of an article has changed
 function notifyTitleChanged(newArticle, existingArticle) {
   telemetry.increment('article_changed');
-  let cat = newArticle.category ? '[' + newArticle.category + ']' : '';
-  let statusText = `${cat} De kop «${existingArticle.title.trim()}» is zojuist gewijzigd naar «${newArticle.title.trim()}» ${newArticle.guid}`;
-  let params = { 
-    status: statusText
+  let cat = newArticle.category ? '[' + newArticle.category + '] ' : '';
+  let statusText = `${cat}De kop «${existingArticle.title.trim()}» is zojuist gewijzigd naar «${newArticle.title.trim()}» ${newArticle.guid}`;
+  let params;
+
+  // Reply to previous Tweet about this title, if it exists
+  if (existingArticle.tweetID) {
+    params = { 
+      status: CONFIG.USERNAME + statusText,
+      in_reply_to_status_id: existingArticle.tweetID
+    }
+  } else {
+    params = { 
+      status: statusText
+    }
   }
 
   T.post('statuses/update', params, function (err, data, response) {
@@ -133,18 +119,21 @@ function notifyTitleChanged(newArticle, existingArticle) {
       telemetry.check("tweet_result", telemetry.CHECKS.WARNING);
       telemetry.increment('error_rate');
     } else {
+      existingArticle.tweetID = response.id_str;
       telemetry.check("tweet_result", telemetry.CHECKS.OK);
       console.log(`Sent out a tweet: ${statusText} -- Oldcat: ${existingArticle.source}, newcat: ${newArticle.source}`);
     }
   })
+
+  // Set old article to new info, otherwise the article would trigger notifyTitleChanged again in the next cycle
+  console.log(`Replacing article at index ${articles.indexOf(existingArticle)} in collection.`);
+  articles[articles.indexOf(existingArticle)] = newArticle;
 }
-
-
 
 function purgeArticles() {
   // Retain articles only when they're no older than 48 hours
   let rightNow = moment(), countPurged = 0, total = articles.length;
-  let twoDaysAgo = rightNow.subtract(2, "days");
+  let twoDaysAgo = rightNow.subtract(CONFIG.PURGE_TRESHOLD, "hours");
   
   articles.filter(article => {
     let theDateThisWasPublished = moment(article.pubDate);
@@ -157,11 +146,20 @@ function purgeArticles() {
   console.log(`Purged ${countPurged} articles (${Math.floor(countPurged / total * 100)}%)`)
 }
 
-function doAfterCheck(totalLength) {
-  telemetry.histogram('article_count', articles.length);
-  telemetry.histogram('feeditems_count', totalLength);
-}
-
 String.prototype.capitalize = function() {
   return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 };
+
+String.prototype.catStrip = function() {
+  return this.replace(/(NOS(\.nl)?)|(nieuws)/gi, '').capitalize().trim();
+}
+
+module.exports = {
+  retrieveArticles: retrieveArticles,
+  purgeArticles: purgeArticles,
+  checkArticleForNewTitle: checkArticleForNewTitle,
+  notifyTitleChanged: notifyTitleChanged,
+  purgeArticles: purgeArticles,
+  capitalize: String.prototype.capitalize,
+  catStrip: String.prototype.catStrip
+}
