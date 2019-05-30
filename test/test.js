377b4c702d064d617f1c70d9fb867ea83b425f58
Fdebijl <18357629+Fdebijl@users.noreply.github.com>
2019-05-30 13:30:30 +0200

    Remove old tests and add mock twitter client

--- test/test.js
+++ test/test.js
@@ -1,75 +1,136 @@
-const assert = require('assert').strict;
+const assert = require('assert');
 const fs = require('fs');
+
 const server = require('../server.js');
-let controlfeed, changedfeed;
-
-let controlitem = {
-  "title": "Nu merken we pas écht hoe gortdroog de zomer was",
-  "link": "http://feeds.nos.nl/~r/nosnieuwsbinnenland/~3/P7voC-07THY/2256743",
-  "pubDate": "Sun, 28 Oct 2018 09:54:22 +0100",
-  "enclosure": {
-      "type": "image/jpeg",
-      "url": "https://nos.nl/data/image/2018/10/28/509210/320x320.jpg"
-  },
-  "content": "<p>Het was droog deze zomer. Extreem droog, maar nog net geen record. Die eer gaat naar de zomer van 1976. Maar dit jaar zijn er dezelfde problemen. Boeren kunnen minder oogsten, schippers kunnen door het lage water veel minder vervoeren en ook de natuur kreeg rake klappen.</p>\r\n<p>NOS op 3 legt in de video hierboven met animaties uit wat de droogte van 2018 betekent. En melkveehouder Maurice, binnenvaartschipper Floris en boswachter Tim vertellen hoe zij door de droogte zijn geraakt. </p><img src=\"http://feeds.feedburner.com/~r/nosnieuwsbinnenland/~4/P7voC-07THY\" height=\"1\" width=\"1\" alt=\"\"/>",
-  "contentSnippet": "Het was droog deze zomer. Extreem droog, maar nog net geen record. Die eer gaat naar de zomer van 1976. Maar dit jaar zijn er dezelfde problemen. Boeren kunnen minder oogsten, schippers kunnen door het lage water veel minder vervoeren en ook de natuur kreeg rake klappen.\r\nNOS op 3 legt in de video hierboven met animaties uit wat de droogte van 2018 betekent. En melkveehouder Maurice, binnenvaartschipper Floris en boswachter Tim vertellen hoe zij door de droogte zijn geraakt.",
-  "guid": "https://nos.nl/l/2256743",
-  "isoDate": "2018-10-28T08:54:22.000Z",
-  "itunes": {},
-  "category": "Binnenlands"
-};
-
-let changeditem = {
-  "title": "Deze zomer was verdomd droog lmao",
-  "link": "http://feeds.nos.nl/~r/nosnieuwsbinnenland/~3/P7voC-07THY/2256743",
-  "pubDate": "Sun, 28 Oct 2018 09:54:22 +0100",
-  "enclosure": {
-      "type": "image/jpeg",
-      "url": "https://nos.nl/data/image/2018/10/28/509210/320x320.jpg"
-  },
-  "content": "<p>Het was droog deze zomer. Extreem droog, maar nog net geen record. Die eer gaat naar de zomer van 1976. Maar dit jaar zijn er dezelfde problemen. Boeren kunnen minder oogsten, schippers kunnen door het lage water veel minder vervoeren en ook de natuur kreeg rake klappen.</p>\r\n<p>NOS op 3 legt in de video hierboven met animaties uit wat de droogte van 2018 betekent. En melkveehouder Maurice, binnenvaartschipper Floris en boswachter Tim vertellen hoe zij door de droogte zijn geraakt. </p><img src=\"http://feeds.feedburner.com/~r/nosnieuwsbinnenland/~4/P7voC-07THY\" height=\"1\" width=\"1\" alt=\"\"/>",
-  "contentSnippet": "Het was droog deze zomer. Extreem droog, maar nog net geen record. Die eer gaat naar de zomer van 1976. Maar dit jaar zijn er dezelfde problemen. Boeren kunnen minder oogsten, schippers kunnen door het lage water veel minder vervoeren en ook de natuur kreeg rake klappen.\r\nNOS op 3 legt in de video hierboven met animaties uit wat de droogte van 2018 betekent. En melkveehouder Maurice, binnenvaartschipper Floris en boswachter Tim vertellen hoe zij door de droogte zijn geraakt.",
-  "guid": "https://nos.nl/l/2256743",
-  "isoDate": "2018-10-28T08:54:22.000Z",
-  "itunes": {},
-  "category": "Binnenlands"
-};
-
-describe('hooks', function() {
-  before(function() {
-    fs.readFile('feed.json', content => {
-      controlfeed = JSON.parse(content);
-    });
-
-    fs.readFile('feed_with_changes.json', content => {
-      changedfeed = JSON.parse(content);
-    });
-  });
-});
+const mocktwit = require('./mocktwit.js');
 
 describe('Helpers', function() {
-  describe('String Methods', function() {
-    it('Should capitalize', function() {
-      let input = "lorem ipsum";
-      let expected = "Lorem Ipsum";
-      let actual = server.capitalize.call(input);
-      assert.deepStrictEqual(actual, expected);
-    });
-
-    it('Should strip cats', function() {
-      let input = "NOS.nl buitenlands nieuws";
-      let expected = "Buitenlands";
-      let actual = server.catStrip.call(input);
-      assert.deepStrictEqual(actual, expected);
-    });
-  });
-
   describe('Auxiliary Methods', function() {
     it('Should generate status text', function() {
-      let expected = "[Binnenlands] De kop «Nu merken we pas écht hoe gortdroog de zomer was» is zojuist gewijzigd naar «Deze zomer was verdomd droog lmao» https://nos.nl/l/2256743";
-      let actual = server.makeStatusText(changeditem, controlitem);
+      const expected = "De kop «Tekort aan zorgpersoneel? 'Dan denk ik: eigen schuld, dikke bult'» is na één dag gewijzigd naar «Tekort aan zorgpersoneel: maar die grijze golf zagen we toch al lang aankomen?» https://nos.nl/l/2277647";
+      const actual = server.makeStatusTextFor(exampleArticle);
       assert.deepStrictEqual(actual, expected);
     });
   });
 });
 
+describe('Main methods', function() {
+  it('Should deny double calls', async function() {
+    const first = await server.notifyTitleChanged(exampleArticle, mocktwit);
+
+    const second = server.notifyTitleChanged(exampleArticle, mocktwit).catch((error) => {
+      assert.deepStrictEqual(error, server.errors.ALREADY_TWEETED);
+      mocktwit.reset();
+    });
+  });
+
+  it('Should send out a tweet', async function() {
+    server.notifyTitleChanged(exampleArticle2, mocktwit).then((statustext) => {
+      const expected = "nothing";
+      const actual = mocktwit.lastRequest.params.status;
+      assert.deepStrictEqual(actual, expected);
+    }).catch((error) => {
+      assert.fail();
+    })
+  });
+
+  it('Should reject calls with less than two titles', async function() {
+    const invalidArticle = exampleArticle;
+    invalidArticle.titles = invalidArticle.titles.slice(0, 1);
+
+    server.notifyTitleChanged(invalidArticle).catch((error) => {
+      assert.deepStrictEqual(error, server.errors.NOT_ENOUGH_TITLES);
+    });
+  });
+
+  it('Should reject unchanged titles', function() {
+    server.notifyTitleChanged(sameTitlesArticle).catch((error) => {
+      assert.deepStrictEqual(error, server.errors.NO_DIFFERENCE);
+    });
+  });
+})
+
+const exampleArticle = {
+  "_id": "5c9a608e74cfe706a614f57d",
+  "org": "NOS",
+  "articleID": "2277647",
+  "feedtitle": "NOS.nl binnenlands nieuws",
+  "sourcefeed": "nosnieuwsbinnenland",
+  "lang": "NL",
+  "link": "http://feeds.nos.nl/~r/nosnieuwsbinnenland/~3/VjhPXN9DyLk/2277647",
+  "guid": "https://nos.nl/l/2277647",
+  "titles": [
+    {
+      "title": "Tekort aan zorgpersoneel? 'Dan denk ik: eigen schuld dikke bult'",
+      "datetime": "March 26th 2019, 12:55:47 pm",
+      "timestamp": 1553621134837
+    },
+    {
+      "title": "Tekort aan zorgpersoneel? 'Dan denk ik: eigen schuld, dikke bult'",
+      "datetime": "March 26th 2019, 6:50:29 pm",
+      "timestamp": 1553622629779
+    },
+    {
+      "title": "Tekort aan zorgpersoneel: maar die grijze golf zagen we toch al lang aankomen?",
+      "datetime": "March 27th 2019, 4:27:00 pm",
+      "timestamp": 1553700420089
+    }
+  ],
+  "first_seen": "March 26th 2019, 6:25:34 pm",
+  "pub_date": "March 26th 2019, 12:55:47 pm"
+}
+
+const exampleArticle2 = {
+  "_id": "5cef1154335fbb1e4b0b328f",
+  "org": "NOS",
+  "articleID": "2286909",
+  "feedtitle": "NOS Nieuws",
+  "sourcefeed": "nosnieuwsalgemeen",
+  "lang": "nl",
+  "link": "https://nos.nl/l/2286909",
+  "guid": "https://nos.nl/l/2286909",
+  "titles": [
+    {
+      "title": "Rondvaartboot kapseist en zinkt in Boedapest, zeker zeven doden",
+      "datetime": "May 30th 2019, 12:54:35 am",
+      "timestamp": 1559171412152
+    },
+    {
+      "title": "Rondvaartboot kapseist en zinkt in Boedapest: zeven doden, 21 vermist",
+      "datetime": "May 30th 2019, 8:03:53 am",
+      "timestamp": 1559196233695
+    },
+    {
+      "title": "Vrees voor tientallen doden na kapseizen toeristenboot in Boedapest",
+      "datetime": "May 30th 2019, 8:57:03 am",
+      "timestamp": 1559199423691
+    }
+  ],
+  "first_seen": "May 30th 2019, 1:10:12 am",
+  "pub_date": "May 30th 2019, 12:54:35 am"
+}
+
+const sameTitlesArticle = {
+  "_id": "5c9a608e74cfe706a614f57d",
+  "org": "NOS",
+  "articleID": "2277647",
+  "feedtitle": "NOS.nl binnenlands nieuws",
+  "sourcefeed": "nosnieuwsbinnenland",
+  "lang": "NL",
+  "link": "http://feeds.nos.nl/~r/nosnieuwsbinnenland/~3/VjhPXN9DyLk/2277647",
+  "guid": "https://nos.nl/l/2277647",
+  "titles": [
+    {
+      "title": "Tekort aan zorgpersoneel? 'Dan denk ik: eigen schuld dikke bult'",
+      "datetime": "March 26th 2019, 12:55:47 pm",
+      "timestamp": 1553621134837
+    },
+    {
+      "title": "Tekort aan zorgpersoneel? 'Dan denk ik: eigen schuld dikke bult'",
+      "datetime": "March 26th 2019, 6:50:29 pm",
+      "timestamp": 1553622629779
+    }
+  ],
+  "first_seen": "March 26th 2019, 6:25:34 pm",
+  "pub_date": "March 26th 2019, 12:55:47 pm"
+}
