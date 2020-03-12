0e8a5cbe5794d8d84601d9b225e37356bfa37d7b
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2020-03-12 01:55:54 +0100

    fix: remove stray files from dist folder

--- dist/config.d.ts
+++
@@ -1,9 +0,0 @@
-import { LOGLEVEL } from '@fdebijl/clog';
-export declare const CONFIG: {
-    MIN_LOGLEVEL: LOGLEVEL;
-    MONGO_URL: string;
-    MOMENT: {
-        LOCALE: string;
-    };
-    PORT: number;
-};
