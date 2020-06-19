86e915772c57bc9d0bf31421ad5d2b37a3b9e000
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2020-06-19 15:32:41 +0200

    refactor: don't send Sentry message on tweet (#57)
    * refactor: don't send Sentry message on tweet
    
    * fix: remove unused import

--- src/twitter/sendTweet.ts
+++ src/twitter/sendTweet.ts
@@ -1,7 +1,6 @@
 import { Clog, LOGLEVEL } from '@fdebijl/clog';
 import Twit from 'twit';
 import { Collection } from 'mongodb';
-import * as Sentry from '@sentry/node';
 
 import { CONFIG } from '../config';
 import { MockTwit, Article, SeenArticle } from '../types';
@@ -20,7 +19,6 @@ export async function sendTweet(collection: Collection, params: Twit.Params, twi
       }
 
       clog.log(`Sent out a tweet: ${params.status}`, LOGLEVEL.DEBUG);
-      Sentry.captureMessage(`Sent out a tweet: ${params.status}`);
 
       if (seenArticle) {
         addTweetToArticle(collection, result as Twit.Twitter.Status, article);
